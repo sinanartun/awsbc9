01a620ecac2142c6b21265eb09f553c44cd25b5c
”sinanartun” <sinanartun@gmail.com>
2024-07-02 20:42:47 +0200

    up

--- 6_1/basic/main.py
+++ 6_1/basic/main.py
@@ -1,9 +1,11 @@
 import time
 from datetime import datetime
+import os
 
 def print_timestamp():
+    name = os.getenv('NAME', 'DefaultName')
     while True:
-        print(datetime.now().isoformat())
+        print(f"{name}: {datetime.now().isoformat()}")
         time.sleep(1)
 
 if __name__ == "__main__":
