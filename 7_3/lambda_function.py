e319b0062b7cdf7fdd92b6e8897db92e6b635b6d
”sinanartun” <sinanartun@gmail.com>
2024-07-11 18:11:24 +0200

    update

--- 7_3/lambda_function.py
+++ 7_3/lambda_function.py
@@ -4,13 +4,12 @@ import pandas as pd
 import joblib
 import os
 import numpy as np
-import sklearn
 from sklearn.exceptions import NotFittedError
-
 
 bucket_name = os.environ['BUCKET_NAME']
 remote_model_key = os.environ['REMOTE_KEY']
-local_model_key = f"/tmp/%s" % remote_model_key
+local_model_key = f"/tmp/{remote_model_key}"
+
 def lambda_handler(event, context):
     # CORS handling
     origin = event['headers'].get('origin', '')
@@ -18,12 +17,15 @@ def lambda_handler(event, context):
     # Validating and extracting query parameters
     query_params = event.get("queryStringParameters", {})
     required_params = ["model", "yil", "km", "renk"]
+
     if not all(param in query_params for param in required_params):
         return {"statusCode": 400, "body": "Missing required query parameters"}
 
     try:
-        model, yil, km, renk = [query_params[param] for param in required_params]
-        yil, km = int(yil), int(km)  # Ensure yil and km are integers
+        model = query_params["model"]
+        yil = int(query_params["yil"])
+        km = int(float(query_params["km"]))  # Handle both int and float inputs
+        renk = query_params["renk"]
     except ValueError:
         return {"statusCode": 400, "body": "Invalid parameter types"}
 
@@ -31,14 +33,10 @@ def lambda_handler(event, context):
     if not os.path.exists(local_model_key):
         boto3.client("s3").download_file(bucket_name, remote_model_key, local_model_key)
 
-
-        
-
-
     # Predict
     try:
         trained_model = joblib.load(local_model_key)
-        input_data = pd.DataFrame([[model, yil, km, renk]])
+        input_data = pd.DataFrame([[model, yil, km, renk]], columns=["model", "yil", "km", "renk"])
         fiyat_prediction = trained_model.predict(input_data)
         fiyat = int(np.round(fiyat_prediction[0]))
     except NotFittedError:
@@ -55,4 +53,3 @@ def lambda_handler(event, context):
         },
         "body": json.dumps({"fiyat": fiyat})
     }
-
